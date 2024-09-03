e2f5627f1a8bb2deb1dd2891ac98e48db5bb2628
Rohit Gupta <49370602+rgrohitgupta938@users.noreply.github.com>
2024-09-03 18:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // write js code here if required
+function timer() {
+	const now = new Date();
+
+	const day = String(now.getDate()).padStart(2,"0");
+	const month = String(now.getMonth()+1).padStart(2,"0");
+	const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+}
+
+function  updateTimer() {
+	const timer = document.getElementById("timer");
+	timer.textContent = timer();
+}
+setInterval(updateTimer,1000);
+updateTimer();
